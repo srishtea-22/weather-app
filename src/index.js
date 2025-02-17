e41f28ca2f7ffad77c91a1327a2504e3743ae1d9
shristy <shristychaudhary4679@gmail.com>
2025-02-17 12:26:05 +0530

    changed search from searchBtn to enter key press

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import './styles.css';
 import { fetchData } from "./modules/api";
 import { display } from './modules/ui';
 
-const searchBtn = document.getElementById("searchBtn");
 const dafaultLocation = "Delhi";
-
+const searchInput = document.querySelector(".search-bar input");
 async function getWeather(location) {
     const weatherData = await fetchData(location);
     display(weatherData);
@@ -14,8 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
     getWeather(dafaultLocation);
 });
 
-searchBtn.addEventListener("click", function() {
-    const userInput = document.querySelector(".search-bar input").value.trim();
-    const location = userInput || defaultLocation;
-    getWeather(location);
-})
+searchInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter"){
+        const userInput = searchInput.value.trim();
+        const location = userInput || defaultLocation;
+        getWeather(location);
+    }
+});
