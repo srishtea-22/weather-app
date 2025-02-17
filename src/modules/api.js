88c0883f5d996af5b2dfa482a3d1cc62081d6b8f
shristy <shristychaudhary4679@gmail.com>
2025-02-17 12:26:49 +0530

    added gif fetch and display logic

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,6 +1,13 @@
-const apiKey = "EJPTQMJK4FKJL4HBSDHYLG6QP";
+const weatherApiKey = "EJPTQMJK4FKJL4HBSDHYLG6QP";
+const giphyApiKey = "5HSR7wiLFbiODi5mYlMILV42shJUzBhw";
 
 export async function fetchData(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${weatherApiKey}`);
     return await response.json();  
+}
+
+export async function fetchGif(icon) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${icon}'`, {mode: 'cors'});
+    const gif =  await response.json();
+    return gif.data.images.original.url;
 }
